1c7094dd035f54f55b7b9f111242e6b1a6affc77
ekager <emilykager@gmail.com>
2019-05-13 11:23:34 -0700

    Reproduce Navigation Dialog Issue With Sample

---
+++ app/src/main/java/com/example/android/codelabs/navigation/MyDialogFragment.kt
@@ -0,0 +1,27 @@
+package com.example.android.codelabs.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
+
+class MyDialogFragment : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.navigate_from_dialog)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null)
+        }
+    }
+}
